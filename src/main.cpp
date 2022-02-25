f73746084cd82e5e3e1dcab820bab2dcf8d70e75
KrishnenduMarathe <kd.workonly.47@gmail.com>
2022-02-25 18:43:38 +0530

    spell corrections

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 ** 	  TicTacToe Game for Terminal for Windows and Linux Platform.
 **
 ** 					 	  	 License
-** 							 -------
+** 					   		 -------
 **
 ** 	  This program is free software: you can redistribute it and/or modify
 **    it under the terms of the GNU General Public License as published by
